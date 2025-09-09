e7c6fa0db74319cb37ddc3e5496f52f28bb339c4
Kal Ahmed <kal.ahmed@epimorphics.com>
2025-09-09 14:52:15 +0100

    Remove obsolete mapping code

--- src/gridded_metadata/mapper.py
+++ src/gridded_metadata/mapper.py
@@ -1,43 +1,12 @@
 
-import json
-
 from rdf_mapper.lib.template_processor import TemplateProcessor
 from rdflib import BNode, Graph, Literal, URIRef
 from rdflib.collection import Collection
-from rdflib.namespace import FOAF, RDF, RDFS, SDO, Namespace, NamespaceManager
+from rdflib.namespace import RDF, RDFS, Namespace
 
 from gridded_metadata.model import Array, Dataset, Dimension, WithAttrs
 
 FDRI = Namespace("http://fdri.ceh.ac.uk/vocab/metadata/")
-
-def _expand_uri(uri_or_curie: str, ns_mgr: NamespaceManager) -> URIRef:
-    if isinstance(uri_or_curie, URIRef):
-        return uri_or_curie
-    if uri_or_curie.startswith("http://") or uri_or_curie.startswith("https://"):
-        return URIRef(uri_or_curie)
-    return ns_mgr.expand_curie(uri_or_curie)
-
-def _expand_mapping(mapping: dict, ns_mgr: NamespaceManager) -> dict:
-    ret = mapping.copy()
-    if 'predicate' in mapping:
-        ret['predicate'] = _expand_uri(mapping['predicate'], ns_mgr)
-    if 'property' in mapping:
-        ret['property'] = _expand_uri(mapping['property'], ns_mgr)
-    return ret
-
-def read_mappings(file_path: str) -> dict:
-    with open(file_path, "r") as f:
-        config = json.load(f)
-    store = Graph()
-    ns_mgr = NamespaceManager(store)
-    for prefix, uri in config.get("namespaces", {}).items():
-        ns_mgr.bind(prefix, Namespace(uri))
-    ret = {}
-    src_mappings = config.get("mappings", {})
-    for mapping_key, mapping_value in src_mappings.items():
-        ret[mapping_key] = _expand_mapping(mapping_value, ns_mgr)
-    return ret
-
 
 class GraphBuilder:
     def __init__(self, dataset: Dataset, base_uri: str, template_processor: TemplateProcessor):
@@ -62,32 +31,6 @@ class GraphBuilder:
         values['$resource'] = element_node
         values['$type'] = type(element).__name__
         self.template_processor.process_row(values)
-
-    def apply_mapping(self, mapping: dict, element: WithAttrs, value: str, element_node: URIRef) -> None:
-        if mapping['type'] == 'literal':
-            predicate = mapping['predicate']
-            self.store.add((element_node, predicate, Literal(value)))
-        elif mapping['type'] == 'agent':
-            predicate = mapping['predicate']
-            name = element.attrs.get(mapping['name'])
-            email = element.attrs.get(mapping['email'])
-            agent_node = BNode()
-            self.store.add((element_node, predicate, agent_node))
-            self.store.add((agent_node, RDF.type, FDRI.Agent))
-            if name:
-                self.store.add((agent_node, RDFS.label, Literal(name)))
-            if email:
-                self.store.add((agent_node, FOAF.mbox, Literal(email)))
-        elif mapping['type'] == 'annotation':
-            property_uri = mapping['property']
-            annotation_node = BNode()
-            self.store.add((annotation_node, RDF.type, FDRI.Annotation))
-            self.store.add((annotation_node, FDRI.property, property_uri))
-            value_node = BNode()
-            self.store.add((annotation_node, FDRI.hasValue, value_node))
-            self.store.add((value_node, RDF.type, SDO.PropertyValue))
-            self.store.add((value_node, SDO.value, Literal(value)))
-            self.store.add((element_node, FDRI.hasAnnotation, annotation_node))
 
     def map_dimensions(self, dataset: Dataset, ds_node: URIRef) -> None:
         for dim in dataset.dimensions.values():
